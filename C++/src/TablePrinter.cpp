da68c10787321d63cae4f0701980357c4e95973d
Hopson97 <mhopson@hotmail.co.uk>
2018-02-25 17:50:26 +0000

    more work on the table

--- C++/src/TablePrinter.cpp
+++ C++/src/TablePrinter.cpp
@@ -2,8 +2,25 @@
 
 TablePrinter::TablePrinter(const std::vector<std::string>& columnNames)
 { 
-    for (auto& column : m_columns)
+    for (auto& column : columnNames)
     {
         m_columns.emplace_back(column);
     }
+}
+
+void TablePrinter::addRow(const std::vector<std::string>&)
+{
+}
+
+void TablePrinter::clear()
+{
+    for (auto& column : m_columns)
+    {
+        m_columns.data.clear();
+    }
+}
+
+void TablePrinter::print()
+{
+
 }
--- C++/src/TablePrinter.h
+++ C++/src/TablePrinter.h
@@ -6,14 +6,21 @@ class TablePrinter
 {
     struct Column
     {
-        Column(std::string name)
-            :   name (name) {}
+        Column(const std::string& title)
+            : title(title)
+        { }
 
-        std::string name;
+        const std::string title;
         std::vector<std::string> data;
     };
     public:
         TablePrinter(const std::vector<std::string>& columnNames);
+
+        void addRow(const std::vector<std::string>& row);
+
+        void clear();
+
+        void print();
 
     private:
         std::vector<Column> m_columns;
